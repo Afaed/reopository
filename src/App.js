a9b67437e8fef88b435543352ad0f7cf2d7bd62d
Afaed <justchoy99@gmail.com>
2022-03-29 20:57:57 -0700

    add user specfic info to workflow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, createContext } from 'react';
+import { StudentProvider, StudentContext } from './utils/StudentContext';
+import StudentList from './components/StudentList';
+import './jass.css';
+import './app.css';
 
+const GlobalStateContext = createContext()
 function App() {
+  const title = ' Providers';
+  useEffect(() => {
+    document.title = title;
+  }, []);
+  const initialState = {
+    students: [
+      {
+      name: "",
+      grade: 100
+      }
+    ],
+    teacher: {}
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>State</h1>
+      <h4 style={{ color: 'lightseagreen' }}>{title}</h4>
+    {/* provides wrap all tge logic*/}
+      <StudentProvider value = {initialState}>
+        <StudentList />
+      </StudentProvider>
     </div>
   );
 }
